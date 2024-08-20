dbc9a5a7cf06e10dd3b4cd2c8ec8ad6646b7fcee
Revathi-Kamineni <104614509+Revathi-Kamineni@users.noreply.github.com>
2024-08-20 21:09:43 +0530

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -21,7 +21,7 @@ public class SeleniumTest {
         System.setProperty("webdriver.chrome.driver", "driver/chromedriver");//linux_64
 
         // Get file
-        File file = new File("IfStatement.html");
+        File file = new File("src/main/IfStatement.html");
         String path = "file://" + file.getAbsolutePath();
 
         // Create a new ChromeDriver instance
